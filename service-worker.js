3e49474b6fbf71b94443c89adf602ad3be7e8dbe
ranshizhuo526-eng <ranshizhuo526@gmail.com>
2026-05-29 16:56:50 +0800

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='instant-thought-inbox-v1';
+const FILES=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
